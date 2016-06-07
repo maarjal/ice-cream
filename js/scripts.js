0144f30c26783262f00eff58cddcc7492ee557cc
Alex Jacks and Maarja Laineste <maarja@>
2016-06-07 09:57:28 -0700

    two kinds of loops demonstrated

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,17 @@ $(document).ready(function() {
   $("#btn").click(function(event) {
     var flavors = ["Mango", "Coconut", "Cookie Dough", "Vanilla", "Pistachio"];
 
-    var counter = 0;
-    flavors.forEach(function(flavor) {
-      $("#flavor").append("<li>" + flavors[counter] + "</li>");
-      counter++;
-    });
+    // var counter = 0;
+    // flavors.forEach(function(flavor) {
+    //   $("#flavor").append("<li>" + flavors[counter] + "</li>");
+    //   counter++;
+    // });
+
+
+    for (var i = 0; i < flavors.length; i++) {
+      $("#flavor").append("<li>" + flavors[i] + "</li>");
+    }
+
 
     $("#flavor").show();
 
